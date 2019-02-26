148b8cde09a6019500cd6a23b7490d5c33f957d0
jeongHooon <pkekzm94@naver.com>
2019-02-26 19:15:01 +0900

    Update Object.cpp

--- Client/TheBoat_client/LabProject08-8/Object.cpp
+++ Client/TheBoat_client/LabProject08-8/Object.cpp
@@ -472,8 +472,8 @@ void CGameObject::LoadFrameHierarchyFromFile(ID3D12Device *pd3dDevice, ID3D12Gra
 	LoadMD5Anim(L"../Assets/Model/RunBackRight.MD5ANIM", NewMD5Model);//12
 	LoadMD5Anim(L"../Assets/Model/RunLeft.MD5ANIM", NewMD5Model);//13
 	LoadMD5Anim(L"../Assets/Model/RunRight.MD5ANIM", NewMD5Model);//14
-	LoadMD5Anim(L"../Assets/Model/CrouchShot.MD5ANIM", NewMD5Model);//15
-	LoadMD5Anim(L"../Assets/Model/CrouchWalk.MD5ANIM", NewMD5Model);//16
+	//LoadMD5Anim(L"../Assets/Model/CrouchShot.MD5ANIM", NewMD5Model);//15
+	//LoadMD5Anim(L"../Assets/Model/CrouchWalk.MD5ANIM", NewMD5Model);//16
 	
 	//LoadMD5Anim(L"../Assets/Model/WarriorAttack.MD5ANIM", NewMD5Model);
 	if (m_ppMeshes[0])
