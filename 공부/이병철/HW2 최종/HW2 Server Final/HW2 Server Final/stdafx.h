f7f159873ea3b86216405d84ba5efb2c48f798e5
leebyeongcheol <lbch7400@naver.com>
2019-03-26 03:17:21 +0900

    수정중

---
+++ 공부/이병철/HW2 최종/HW2 Server Final/HW2 Server Final/stdafx.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#pragma comment (lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int ClientCount = 0;
+BOOL isStart = false;
+
+POINT characterPos1 = { 400,0 }; //캐릭터 초기 위치 값
+POINT characterPos2 = { 400,700 };
+
+#define PLAYER1 1
+#define PLAYER2 2
+
+#define BOARDNUM 8
+#define BOARDSIZE 800
+#define CHARACTERSIZE 50
+
+BOOL P1KeyBuffer[256];
+BOOL P2KeyBuffer[256];
+
+
