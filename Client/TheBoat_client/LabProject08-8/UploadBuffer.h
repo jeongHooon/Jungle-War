4e7cd1e9dc855b25799fa861cd1242ee06367d09
jeongHooon <pkekzm94@naver.com>
2019-04-14 20:30:55 +0900

    Update UploadBuffer.h

--- Client/TheBoat_client/LabProject08-8/UploadBuffer.h
+++ Client/TheBoat_client/LabProject08-8/UploadBuffer.h
@@ -10,9 +10,7 @@ public:
     {
         mElementByteSize = sizeof(T);
 
-        // Constant buffer elements need to be multiples of 256 bytes.
-        // This is because the hardware can only view constant data 
-        // at m*256 byte offsets and of n*256 byte lengths. 
+     
         // typedef struct D3D12_CONSTANT_BUFFER_VIEW_DESC {
         // UINT64 OffsetInBytes; // multiple of 256
         // UINT   SizeInBytes;   // multiple of 256
