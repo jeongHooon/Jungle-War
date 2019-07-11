089c0fc2e7f8f1cfaa3e49167a26bbbb0b9ffcc3
sujin72 <tnwlsgn@naver.com>
2019-07-11 14:30:28 +0900

    no message

---
+++ DB/DB.cpp
@@ -0,0 +1,93 @@
+
+// SQLBindCol_ref.cpp  
+// compile with: odbc32.lib  
+#include <windows.h>  
+#include <stdio.h>  
+#include <iostream>
+
+using namespace std;
+
+#define UNICODE  
+#include <sqlext.h>  
+
+#define NAME_LEN 50  
+#define PHONE_LEN 20  
+
+void show_error() {
+	printf("error\n");
+}
+
+
+
+int main() {
+	SQLHENV henv;
+	SQLHDBC hdbc;
+	SQLHSTMT hstmt = 0;
+	SQLRETURN retcode;
+	SQLWCHAR uname[NAME_LEN];
+	SQLINTEGER uid, ulevel;
+	SQLLEN cb_uname = 0, cb_uid = 0, cb_ulevel = 0;
+
+	setlocale(LC_ALL, "korean");
+
+	// Allocate environment handle  
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+	// Set the ODBC version environment attribute  
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+
+		// Allocate connection handle  
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+			// Set login timeout to 5 seconds  
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+				// Connect to data source  
+				retcode = SQLConnect(hdbc, (SQLWCHAR*)L"2019_WT_GS", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+
+				// Allocate statement handle  
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					cout << "Database Connect OK\n";
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+
+					retcode = SQLExecDirect(hstmt, (SQLWCHAR *)L"SELECT user_id, user_name, user_level FROM user_table ORDER BY 2, 1, 3", SQL_NTS);
+
+
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+
+						// Bind columns 1, 2, and 3  
+						retcode = SQLBindCol(hstmt, 1, SQL_INTEGER, &uid, 100, &cb_uid);
+						retcode = SQLBindCol(hstmt, 2, SQL_C_CHAR, uname, NAME_LEN, &cb_uname);
+						retcode = SQLBindCol(hstmt, 3, SQL_INTEGER, &ulevel, PHONE_LEN, &cb_ulevel);
+
+						// Fetch and print each row of data. On an error, display a message and exit.  
+						for (int i=0; ; i++) {
+							retcode = SQLFetch(hstmt);
+							if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO)
+								show_error();
+							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+								wprintf(L"%d: %d %S %d\n", i + 1, uid, uname, ulevel);
+							else
+								break;
+						}
+					}
+
+					// Process data  
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						SQLCancel(hstmt);
+						SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+					}
+
+					SQLDisconnect(hdbc);
+				}
+
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			}
+		}
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+	system("pause");
+}
